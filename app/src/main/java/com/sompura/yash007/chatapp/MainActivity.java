15a2c6d3e106bf608d759aa6afbb7ef0902f6e7e
Yash Sompura <sompurayash@live.com>
2018-10-21 17:12:37 -0400

    [DESIGN] Login Screen (Main Activity)

--- app/src/main/java/com/sompura/yash007/chatapp/MainActivity.java
+++ app/src/main/java/com/sompura/yash007/chatapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sompura.yash007.chatapp;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
     }
 }
