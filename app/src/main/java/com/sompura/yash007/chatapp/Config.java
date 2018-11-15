633f640aafd1ced44d135754789f87a91aac8cc2
Yash Sompura <sompurayash@live.com>
2018-11-15 14:26:51 -0500

    [Code] Configuration Class for Web REST API

---
+++ app/src/main/java/com/sompura/yash007/chatapp/Config.java
@@ -0,0 +1,25 @@
+package com.sompura.yash007.chatapp;
+
+public class Config {
+
+    public static String webServiceRoot = "http://chatapp.cptrivedi.com/";
+
+    //Users class web rest links
+    public static String login = webServiceRoot + "users.php?login=true";
+    public static String signUp = webServiceRoot + "users.php?signUp=true";
+    public static String update = webServiceRoot + "users.php?updateUser=true";
+
+    //Contact class web rest links
+    public static String addContact = webServiceRoot + "contacts.php?addContact=true";
+    public static String removeContact = webServiceRoot + "contacts.php?removeContact=true";
+    public static String listContacts = webServiceRoot + "contacts.php?listContacts=true";
+    public static String findContacts = webServiceRoot + "contacts.php?findContacts=true";
+
+    //Chat class web rest links
+    public static String chatList = webServiceRoot + "chat.php?chatList=true";
+    public static String sendChat = webServiceRoot + "chat.php?sendChat=true";
+    public static String receiveChat = webServiceRoot + "chat.php?receiveChat=true";
+    public static String findTotalMessages = webServiceRoot + "chat.php?findTotalMessages=true";
+    public static String loadChat = webServiceRoot + "chat.php?loadChat=true";
+
+}
