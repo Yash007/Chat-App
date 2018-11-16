be4d32f4497b3e8de15208cab2aa4bcf88e7b3b7
Yash Sompura <sompurayash@live.com>
2018-11-15 20:34:04 -0500

    [Task] Find Contacts implementation

--- app/src/main/java/com/sompura/yash007/chatapp/ContactsFragment.java
+++ app/src/main/java/com/sompura/yash007/chatapp/ContactsFragment.java
@@ -1,8 +1,11 @@
 package com.sompura.yash007.chatapp;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,9 +14,28 @@ import android.view.ViewGroup;
 
 
 public class ContactsFragment extends Fragment {
+
+    FloatingActionButton addContacts;
+    Activity context;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
+        context = getActivity();
+        View view = inflater.inflate(R.layout.fragment_contacts, container, false);
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        addContacts = (FloatingActionButton) context.findViewById(R.id.addContactsFloatingButton);
+
+        addContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(context, SearchContactsActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/sompura/yash007/chatapp/SearchContactsActivity.java
+++ app/src/main/java/com/sompura/yash007/chatapp/SearchContactsActivity.java
@@ -1,13 +1,212 @@
 package com.sompura.yash007.chatapp;
 
+import android.app.ProgressDialog;
+import android.app.SearchManager;
+import android.os.AsyncTask;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class SearchContactsActivity extends AppCompatActivity {
 
+    private ProgressDialog pDialog;
+    private ListView lv;
+    ArrayList<HashMap<String, String>> contactList;
+    private static final String TAG = "SearchContactsActivity";
+    android.support.v7.widget.SearchView searchView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_contacts);
+
+        contactList = new ArrayList<>();
+        lv = (ListView) findViewById(R.id.searchContactsResult);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_contacts, menu);
+        final MenuItem searchItem = menu.findItem(R.id.searchContact);
+
+        if (searchItem != null) {
+            searchView = (android.support.v7.widget.SearchView) MenuItemCompat.getActionView(searchItem);
+            searchView.setOnCloseListener(new android.support.v7.widget.SearchView.OnCloseListener() {
+                @Override
+                public boolean onClose() {
+                    return false;
+                }
+            });
+
+            searchView.setOnSearchClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+
+            EditText searchPlate = (EditText) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+            searchPlate.setHint("Search");
+            View searchPlateView = searchView.findViewById(android.support.v7.appcompat.R.id.search_plate);
+            searchPlateView.setBackgroundColor(ContextCompat.getColor(SearchContactsActivity.this, android.R.color.transparent));
+            // use this method for search process
+            searchView.setOnQueryTextListener(new android.support.v7.widget.SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    //async code will be here
+                    contactList.clear();
+
+                    if(query != null && !query.isEmpty())
+                        new SearchPlayers(query).execute();
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    contactList.clear();
+                    //async code will be here
+
+                    if (newText != null && !newText.isEmpty())
+                        new SearchPlayers(newText).execute();
+                    return false;
+                }
+            });
+            SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (!searchView.isIconified()) {
+            searchView.setIconified(true);
+            findViewById(R.id.searchContactsActivity).setVisibility(View.VISIBLE);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private class SearchPlayers extends AsyncTask<Void, Void, Void> {
+
+        String name;
+        public SearchPlayers(String name) {
+            this.name = name;
+        }
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(SearchContactsActivity.this);
+            pDialog.setMessage("Please wait...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+
+        }
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(Config.findContacts+"&q="+name);
+
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+
+                    // Getting JSON Array node
+                    JSONArray contacts = jsonObj.getJSONArray("contacts");
+
+                    // looping through All Contacts
+                    for (int i = 0; i < contacts.length(); i++) {
+                        JSONObject c = contacts.getJSONObject(i);
+
+                        String id = c.getString("uId");
+                        String name = c.getString("cName");
+                        String email = c.getString("cEmail");
+                        String shortName = c.getString("cShortName");
+                        String phone = c.getString("cMobile");
+
+                        // tmp hash map for single contact
+                        HashMap<String, String> contact = new HashMap<>();
+
+                        // adding each child node to HashMap key => value
+                        contact.put("id", id);
+                        contact.put("name",name);
+                        contact.put("email",email);
+                        contact.put("shortName",shortName);
+                        contact.put("phone",phone);
+
+                        // adding contact to contact list
+                        contactList.add(contact);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server. Check LogCat for possible errors!",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+            ListAdapter adapter = new SimpleAdapter(
+                    SearchContactsActivity.this, contactList,
+                    R.layout.contacts_listview, new String[]{"id","name", "email","phone","shortName"},
+                    new int[]{R.id.contactIdListView,R.id.nameListView,
+                    R.id.emailListView,R.id.phoneListView,R.id.shortNameListView});
+
+            lv.setAdapter(adapter);
+        }
+
     }
 }
