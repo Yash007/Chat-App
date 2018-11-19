8cfe1eef60414842c90bf567dacee591b0e96d0d
Yash Sompura <sompurayash@live.com>
2018-11-18 21:41:56 -0500

    [AES] AES Implementation for Encryption and decryption

--- app/src/main/java/com/sompura/yash007/chatapp/DashboardActivity.java
+++ app/src/main/java/com/sompura/yash007/chatapp/DashboardActivity.java
@@ -1,12 +1,19 @@
 package com.sompura.yash007.chatapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    SharedPreferences.Editor sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +23,8 @@ public class DashboardActivity extends AppCompatActivity {
         tabLayout.addTab(tabLayout.newTab().setText("Chat"));
         tabLayout.addTab(tabLayout.newTab().setText("Contacts"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        sharedPreferences = getSharedPreferences(Config.prefName,MODE_PRIVATE).edit();
 
         final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
         final PageAdapter adapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
@@ -38,4 +47,20 @@ public class DashboardActivity extends AppCompatActivity {
             }
         });
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_dashboard, menu);
+//        final MenuItem signOut = menu.findItem(R.id.signOut);
+//
+//        if(signOut != null) {
+//            if(signOut.getItemId() == R.id.signOut) {
+//                sharedPreferences.putBoolean("keepLogin",false);
+//                sharedPreferences.commit();
+//                sharedPreferences.apply();
+//                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+//            }
+//        }
+//        return super.onCreateOptionsMenu(menu);
+//    }
 }
